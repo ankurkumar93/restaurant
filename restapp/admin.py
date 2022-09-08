2276a4b6f4335763ecfd066e27b49d646b2547ba
Ankur Kumar <ankurkumar.balrkishna@impiger.com>
2022-09-08 21:55:09 +0530

    registered models in admin file

--- restapp/admin.py
+++ restapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(RestaurantProfile)
+admin.site.register(Bill)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+
+
