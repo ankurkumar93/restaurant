567d592cd0c3aceca51373492fe3d630baac6a6c
Ankur Kumar <ankurkumar.balrkishna@impiger.com>
2022-09-08 21:48:19 +0530

    updated db settings

--- rest/settings.py
+++ rest/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'rest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'restaurant',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'restaurant',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
